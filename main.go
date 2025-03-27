ccfb6b3e1f97805a985a5b455defa279c8909af4
Avi Deitcher <avi@deitcher.net>
2025-03-26 20:27:40 -0700

    add support for creating and downloading images
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -19,6 +20,8 @@ const (
 	KB = 1024
 	MB = 1024 * KB
 	GB = 1024 * MB
+
+	blockSize = 4096
 )
 
 var (
@@ -42,6 +45,11 @@ type Node struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	IP   string `json:"ip"`
+}
+
+type Image struct {
+	Name   string `json:"name"`
+	Source string `json:"source"`
 }
 
 // loadOxideToken retrieves the API token from persistent storage
@@ -119,47 +127,180 @@ runcmd:
 // ensureImagesExist checks if the right images exist and creates them if needed
 // they can exist at the silo or project level. However, if they do not exist, then they
 // will be created at the project level.
-func ensureImagesExist(ctx context.Context, client *oxide.Client, projectID string, imageNames ...string) ([]string, error) {
-	images, err := client.ImageList(ctx, oxide.ImageListParams{Project: oxide.NameOrId(projectID)})
+func ensureImagesExist(ctx context.Context, client *oxide.Client, projectID string, images ...Image) ([]string, error) {
+	existing, err := client.ImageList(ctx, oxide.ImageListParams{Project: oxide.NameOrId(projectID)})
 	if err != nil {
 		return nil, fmt.Errorf("failed to list images: %w", err)
 	}
 	var (
-		missingImages []string
+		missingImages []Image
 		imageMap      = make(map[string]string)
 		idMap         = make(map[string]string)
 	)
-	for _, image := range images.Items {
+	for _, image := range existing.Items {
 		imageMap[string(image.Name)] = image.Id
 	}
-	for _, imageName := range imageNames {
-		if _, ok := imageMap[imageName]; !ok {
-			missingImages = append(missingImages, imageName)
+	for _, image := range images {
+		if _, ok := imageMap[image.Name]; !ok {
+			missingImages = append(missingImages, image)
 		} else {
-			idMap[imageName] = imageMap[imageName]
+			idMap[image.Name] = imageMap[image.Name]
 		}
 	}
 
 	for _, missingImage := range missingImages {
-		// how to create the image?
-		image, err := client.ImageCreate(ctx, oxide.ImageCreateParams{})
+		snapshotName := fmt.Sprintf("snapshot-%s", missingImage.Name)
+		// how to create the image? oxide makes this a bit of a pain, you need to do multiple steps:
+		// 1. Download the image from the URL locally to a temporary file
+		// 2. Determine the size of the image
+		// 3. Create a blank disk of that size or larger, rounded up to nearest block size at least
+		//      https://docs.oxide.computer/api/disk_create
+		// 4. Import base64 blobs of data from the disk to the blank disk
+		//      https://docs.oxide.computer/api/disk_bulk_write_import_start
+		//      https://docs.oxide.computer/api/disk_bulk_write_import
+		// 	    https://docs.oxide.computer/api/disk_bulk_write_import_stop
+		// 5. Finalize the import by making a snapshot of the disk
+		//      https://docs.oxide.computer/api/disk_finalize_import
+		// 6. Create an image from the snapshot
+		//      https://docs.oxide.computer/api/image_create
+		file, err := os.CreateTemp("", "image-")
+		if err != nil {
+			return nil, fmt.Errorf("failed to create temporary file: %w", err)
+		}
+		defer os.RemoveAll(file.Name())
+		if err := downloadFile(file.Name(), missingImage.Source); err != nil {
+			return nil, fmt.Errorf("failed to download image: %w", err)
+		}
+		stat, err := file.Stat()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get file size: %w", err)
+		}
+		size := stat.Size()
+		if size == 0 {
+			return nil, fmt.Errorf("image file is empty")
+		}
+		// round up to nearest block size
+		size = (size + blockSize) &^ blockSize
+		// create the disk
+		disk, err := client.DiskCreate(ctx, oxide.DiskCreateParams{
+			Project: oxide.NameOrId(projectID),
+			Body: &oxide.DiskCreate{
+				Description: fmt.Sprintf("Disk for image '%s'", missingImage.Name),
+				Size:        oxide.ByteCount(size),
+				Name:        oxide.Name(missingImage.Name),
+				DiskSource:  oxide.DiskSource{Type: oxide.DiskSourceTypeBlank},
+			},
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to create disk: %w", err)
+		}
+		// import the data
+		if err := client.DiskBulkWriteImportStart(ctx, oxide.DiskBulkWriteImportStartParams{
+			Project: oxide.NameOrId(projectID),
+			Disk:    oxide.NameOrId(disk.Id),
+		}); err != nil {
+			return nil, fmt.Errorf("failed to start bulk write import: %w", err)
+		}
+		// write in 1MB chunks or until finished
+		f, err := os.Open(file.Name())
+		if err != nil {
+			return nil, fmt.Errorf("failed to open file: %w", err)
+		}
+		defer f.Close()
+		var offset int
+		for {
+			buf := make([]byte, MB)
+			n, err := f.Read(buf)
+			if err != nil {
+				return nil, fmt.Errorf("failed to read file: %w", err)
+			}
+			if n == 0 {
+				break
+			}
+			// convert the read data into base64. Why? because that is what oxide wants
+			if err := client.DiskBulkWriteImport(ctx, oxide.DiskBulkWriteImportParams{
+				Project: oxide.NameOrId(projectID),
+				Disk:    oxide.NameOrId(disk.Id),
+				Body: &oxide.ImportBlocksBulkWrite{
+					Base64EncodedData: base64.StdEncoding.EncodeToString(buf[:n]),
+					Offset:            offset,
+				},
+			}); err != nil {
+				return nil, fmt.Errorf("failed to write data: %w", err)
+			}
+			offset += n
+		}
+		if err := client.DiskBulkWriteImportStop(ctx, oxide.DiskBulkWriteImportStopParams{
+			Project: oxide.NameOrId(projectID),
+			Disk:    oxide.NameOrId(disk.Id),
+		}); err != nil {
+			return nil, fmt.Errorf("failed to stop bulk write import: %w", err)
+		}
+		// finalize the import
+		if err := client.DiskFinalizeImport(ctx, oxide.DiskFinalizeImportParams{
+			Disk:    oxide.NameOrId(disk.Id),
+			Project: oxide.NameOrId(projectID),
+			Body: &oxide.FinalizeDisk{
+				SnapshotName: oxide.Name(snapshotName),
+			},
+		}); err != nil {
+			return nil, fmt.Errorf("failed to finalize import: %w", err)
+		}
+
+		client.DiskDelete(ctx, oxide.DiskDeleteParams{
+			Project: oxide.NameOrId(projectID),
+			Disk:    oxide.NameOrId(disk.Id),
+		})
+
+		image, err := client.ImageCreate(ctx, oxide.ImageCreateParams{
+			Project: oxide.NameOrId(projectID),
+			Body: &oxide.ImageCreate{
+				Name:        oxide.Name(missingImage.Name),
+				Description: fmt.Sprintf("Image for '%s'", missingImage.Name),
+				Source: oxide.ImageSource{
+					Type: oxide.ImageSourceTypeSnapshot,
+					Id:   snapshotName,
+				},
+			},
+		})
 		if err != nil {
 			return nil, fmt.Errorf("failed to create image: %w", err)
 		}
-		imageMap[missingImage] = image.Id
-		idMap[missingImage] = image.Id
+		imageMap[missingImage.Name] = image.Id
+		idMap[missingImage.Name] = image.Id
 	}
 
 	// go through all of the image names and get their IDs
 	var ids []string
-	for _, imageName := range imageNames {
-		if id, ok := imageMap[imageName]; !ok {
-			return nil, fmt.Errorf("image '%s' does not exist", imageName)
+	for _, image := range images {
+		if id, ok := imageMap[image.Name]; !ok {
+			return nil, fmt.Errorf("image '%s' does not exist", image.Name)
 		} else {
 			ids = append(ids, id)
 		}
 	}
 	return ids, nil
+}
+
+// downloadFile downloads a file from a URL and saves it to the local filesystem
+// It should understand different file URL schemes, but for now, just knows https
+func downloadFile(filepath, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download file: %w", err)
+	}
+	defer resp.Body.Close()
+
+	f, err := os.Create(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer f.Close()
+
+	if _, err := f.ReadFrom(resp.Body); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+	return nil
 }
 
 // ensureProjectExists checks if the right project exists and returns its ID
@@ -353,7 +494,7 @@ func initializeSetup() error {
 		return fmt.Errorf("project verification failed: %v", err)
 	}
 
-	if _, err := ensureImagesExist(ctx, client, projectID, controlPlaneImageName, workerImageName); err != nil {
+	if _, err := ensureImagesExist(ctx, client, projectID, Image{controlPlaneImageName, controlPlaneImageSource}, Image{workerImageName, workerImageSource}); err != nil {
 		return fmt.Errorf("image verification failed: %v", err)
 	}
 
